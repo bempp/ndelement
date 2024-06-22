27d1f4f2e99a445e827e592091bccd14351ad10b
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2024-06-22 14:45:59 +0100

    Copy elements from bempp-rs (#2)
    * Add FiniteElement, ElementFamily
    
    * Copy elements from bempp-rs
    
    * use quadrature from bempp in test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
 //! n-dimensional grid
 #![cfg_attr(feature = "strict", deny(warnings))]
 #![warn(missing_docs)]
+
+pub mod ciarlet;
+pub mod polynomials;
+pub mod reference_cell;
+pub mod traits;
+pub mod types;
+
+#[cfg(test)]
+mod test {
+    extern crate blas_src;
+    extern crate lapack_src;
+}
