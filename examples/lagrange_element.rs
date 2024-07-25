6dd55a149385f8ad6e81898a35477741512e3241
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2024-07-25 08:10:38 +0100

    update examples

--- examples/lagrange_element.rs
+++ examples/lagrange_element.rs
@@ -17,9 +17,9 @@ fn main() {
     // Create an array to store the basis function values
     let mut basis_values = rlst_dynamic_array4!(f64, element.tabulate_array_shape(0, 1));
     // Create array containing the point [1/3, 1/3]
-    let mut points = rlst_dynamic_array2!(f64, [1, 2]);
+    let mut points = rlst_dynamic_array2!(f64, [2, 1]);
     points[[0, 0]] = 1.0 / 3.0;
-    points[[0, 1]] = 1.0 / 3.0;
+    points[[1, 0]] = 1.0 / 3.0;
     // Tabulate the element's basis functions at the point
     element.tabulate(&points, 0, &mut basis_values);
     println!(
@@ -29,7 +29,7 @@ fn main() {
 
     // Set point to [1, 0]
     points[[0, 0]] = 1.0;
-    points[[0, 1]] = 0.0;
+    points[[1, 0]] = 0.0;
     // Tabulate the element's basis functions at the point
     element.tabulate(&points, 0, &mut basis_values);
     println!(
