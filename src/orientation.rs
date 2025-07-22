6bdc21ebd36423fedbe1fb462e4033c56c44cda5
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2025-07-22 10:40:02 +0100

    fmt

--- src/orientation.rs
+++ src/orientation.rs
@@ -1,6 +1,6 @@
 //! Cell orientation
 
-use crate::{types::ReferenceCellType, reference_cell};
+use crate::{reference_cell, types::ReferenceCellType};
 use itertools::izip;
 
 /// Compute a 32-bit integer that encodes the orientation differences between the reference cell and a cell with vertices numbered as input
@@ -19,26 +19,37 @@ pub fn compute_orientation(entity_type: ReferenceCellType, vertices: &[usize]) -
         }
     }
     if dim > 2 {
-        for (t, v) in izip!(&reference_cell::entity_types(entity_type)[2], reference_cell::faces(entity_type)) {
+        for (t, v) in izip!(
+            &reference_cell::entity_types(entity_type)[2],
+            reference_cell::faces(entity_type)
+        ) {
             println!("{t:?} {v:?}");
             match t {
                 ReferenceCellType::Triangle => {
-                    let m = v.iter().map(|i| vertices[*i]).enumerate()
+                    let m = v
+                        .iter()
+                        .map(|i| vertices[*i])
+                        .enumerate()
                         .min_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
-                        .map(|(index, _)| index).unwrap() as i32;
+                        .map(|(index, _)| index)
+                        .unwrap() as i32;
                     orientation += m << n;
                     n += 2;
                     let next = vertices[v[(m as usize + 1) % 3]];
-                    let prev = vertices[v[if m == 0 { 2 } else { m as usize - 1}]];
+                    let prev = vertices[v[if m == 0 { 2 } else { m as usize - 1 }]];
                     if next > prev {
                         orientation += 1 << n;
                     }
                     n += 1;
                 }
                 ReferenceCellType::Quadrilateral => {
-                    let m = v.iter().map(|i| vertices[*i]).enumerate()
+                    let m = v
+                        .iter()
+                        .map(|i| vertices[*i])
+                        .enumerate()
                         .min_by(|(_, a), (_, b)| a.partial_cmp(b).unwrap())
-                        .map(|(index, _)| index).unwrap() as i32;
+                        .map(|(index, _)| index)
+                        .unwrap() as i32;
                     if m == 2 {
                         orientation += 3 << n;
                     } else if m == 3 {
@@ -47,14 +58,16 @@ pub fn compute_orientation(entity_type: ReferenceCellType, vertices: &[usize]) -
                         orientation += m << n;
                     }
                     n += 2;
-                    let next = vertices[v[ [1, 3, 0, 2][m as usize] ]];
-                    let prev = vertices[v[ [2, 0, 3, 1][m as usize] ]];
+                    let next = vertices[v[[1, 3, 0, 2][m as usize]]];
+                    let prev = vertices[v[[2, 0, 3, 1][m as usize]]];
                     if next > prev {
                         orientation += 1 << n;
                     }
                     n += 1;
                 }
-                _ => { panic!("Unsupported face type: {t:?}"); }
+                _ => {
+                    panic!("Unsupported face type: {t:?}");
+                }
             }
         }
     }
@@ -68,10 +81,7 @@ mod test {
 
     #[test]
     fn test_compute_orientations_interval() {
-        for (v, o) in [
-            ([0, 1], 0),
-            ([1, 0], 0),
-        ] {
+        for (v, o) in [([0, 1], 0), ([1, 0], 0)] {
             assert_eq!(compute_orientation(ReferenceCellType::Interval, &v), o);
         }
     }
@@ -115,7 +125,7 @@ mod test {
         for (v, o) in [
             ([0, 1, 2, 3], 0),
             ([0, 3, 2, 1], 149895), // 100 100 100 110 000111
-            ([1, 2, 0, 3], 68436), // 010 000 101 101 010100
+            ([1, 2, 0, 3], 68436),  // 010 000 101 101 010100
             ([1, 3, 2, 0], 140687), // 100 010 010 110 001111
         ] {
             assert_eq!(compute_orientation(ReferenceCellType::Tetrahedron, &v), o);
@@ -127,9 +137,8 @@ mod test {
         for (v, o) in [
             ([0, 1, 2, 3, 4, 5, 6, 7], 0),
             ([2, 5, 6, 1, 7, 0, 4, 3], 165899128), // 001 001 111 000 110 110 101101111000
-            ([4, 1, 5, 0, 2, 3, 6, 7], 95215661), // 000 101 101 011 001 110 000000101101
+            ([4, 1, 5, 0, 2, 3, 6, 7], 95215661),  // 000 101 101 011 001 110 000000101101
             ([6, 5, 1, 7, 2, 4, 3, 0], 306691223), // 010 010 010 001 111 011 110010010111
-
         ] {
             assert_eq!(compute_orientation(ReferenceCellType::Hexahedron, &v), o);
         }
